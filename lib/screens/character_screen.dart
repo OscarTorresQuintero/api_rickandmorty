39ff28063d2d448f4ea6bd864d09dfede9a42da7
Oscar Quintero <oscartquintero23@gmail.com>
2026-03-20 08:41:41 -0500

    Se agrega diseño básico de la pantalla de personaje

--- lib/screens/character_screen.dart
+++ lib/screens/character_screen.dart
@@ -5,12 +5,26 @@ class CharacterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Personaje"),
       ),
-      body: const Center(
-        child: Text("Detalle del personaje"),
+      body: Column(
+        children: [
+          SizedBox(
+            height: size.height * 0.35,
+            width: double.infinity,
+            child: Image.network(
+              "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Text("Rick Sanchez"),
+          const Text("Status: Alive"),
+        ],
       ),
     );
   }
